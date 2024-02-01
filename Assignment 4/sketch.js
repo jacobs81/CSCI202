9c6b4552cc5cca94bacd1bbb0d49742a227eb1e9
jacobs81 <cshack11500@gmail.com>
2024-01-31 16:05:40 -0800

    Finished making and coloring shapes. Need to style page and create an either animated or interactive version of the painting.

--- Assignment 4/sketch.js
+++ Assignment 4/sketch.js
@@ -4,23 +4,38 @@ function setup() {
 }
 
 function draw() {
-    strokeWeight(0);
+    strokeWeight(0.5);
+    // stroke();
     var middleX = width / 2;
     var middleY = height / 2;
     background(200);
     fill(169,169,169)
     rect(middleX, middleY, width, height)
+
+
+    // Tan Triangle
+    fill(255,255,153);
+    triangle(0, middleY, middleX - .10 * width, middleY, 0, height);
+
+
+
+    // Two light Gray Triangles
+    fill(224,224,224);
+    // Upper
+    triangle(middleX - .10 * width, middleY + .10 * height, middleX - .10 * width, -55, width, middleY);
+    // Lower
+    triangle(middleX - .10 * width, height + 50, -5, height, middleX - .10 * width, middleY );
+
+
+
+
+
     // Red Triangle
     fill(255,0,0);
     triangle(width, -5, middleX - .10 * width, middleY + .10 * height, width, height + .15 * height);
 
     // Black Triangle
-    fill(0,255,0);
-    triangle(middleX - .10 * width, -10, middleX - .10 * width, height + .10 * height, -50, middleY);
-
-    // Tan Triangle
-    fill(255,255,153);
-    triangle(25,25,25,25,25,25);
-    // triangle((middleX - .10 * width) / 2, (height + .10 * height ) * 1.5, -50, middleY, height)
+    fill(0,0,0);
+    triangle(middleX - .10 * width, -10, middleX - .10 * width, height - 10, -50, middleY);
     
 }
