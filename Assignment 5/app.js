0c550953611eeceed4c86a519d655dfa4e8cf9a0
jacobs81 <cshack11500@gmail.com>
2024-02-06 21:55:31 -0800

    proof of concept

---
+++ Assignment 5/app.js
@@ -0,0 +1,39 @@
+// function fetchAndDisplayData() {
+//     fetch("building_info.json") 
+//         .then(response => response.json())
+//         .then(data => {
+//             const AWValue = data.AW; 
+//             const awDiv = document.querySelector(".AW");
+//             awDiv.textContent = "There are " + AWValue + " people in AW";
+//         })
+//         .catch(error => console.error("Error fetching data:", error));
+// }
+
+// fetchAndDisplayData();
+
+// setInterval(fetchAndDisplayData, 5000);
+
+
+function fetchAndDisplayData() {
+    fetch("building_info.json") 
+        .then(response => response.json())
+        .then(data => {
+            // Loop through each key in the data object
+            for (const key in data) {
+                if (Object.hasOwnProperty.call(data, key)) {
+                    const value = data[key];
+                    const div = document.querySelector(`.${key}`); // Select div based on class name
+                    if (div) {
+                        div.textContent = `There are ${value} people in ${key}`;
+                    } else {
+                        console.error(`Div with class ${key} not found.`);
+                    }
+                }
+            }
+        })
+        .catch(error => console.error("Error fetching data:", error));
+}
+
+fetchAndDisplayData();
+
+setInterval(fetchAndDisplayData, 5000);
