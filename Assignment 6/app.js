7d7aa6006b21e94a56242de564e449ce8422f7cd
jacobs81 <cshack11500@gmail.com>
2024-02-14 00:08:03 -0800

    Added the javascript for all the information from the API. Displaying name and current spacestation the astronaut is on.

---
+++ Assignment 6/app.js
@@ -0,0 +1,20 @@
+// Information about each astronaut in space
+$.getJSON('http://api.open-notify.org/astros.json', function (data) {
+    console.log(data);
+    console.log(data["people"]);
+
+    for (let x = 0; x < data["people"].length; x++) {
+        var name = data["people"][x]["name"];
+        var abroad = data["people"][x]["craft"];
+        console.log("Name: " + name + " and Abroad: " + abroad);
+        console.log(x);
+        var temp = document.getElementById(x);
+        temp.innerHTML = name + "<br>" + abroad
+    }
+});
+
+
+// Information about where the ISS exactly is
+$.getJSON('http://api.open-notify.org/iss-now.json', function (data) {
+    console.log(data);
+});
